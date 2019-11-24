5eb5ab30a23d944a8294154a317a4e50b4515fa9
Ron Artenstein <ronartenstein@gmail.com>
2019-11-24 17:29:14 +0200

    add new/create action and view, using simple form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,14 @@ class FoodsController < ApplicationController
   def show
     @food = Food.find(params[:id])
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(params[:id])
+    @food.save
+    redirect_to foods_path
+  end
 end
